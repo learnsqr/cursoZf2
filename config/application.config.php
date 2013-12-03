a647aad444e8315cabbdd2757ac84e4c968da5ff
antonov <artem@antonov.es>
2013-12-03 19:59:02 +0100

    BjyAuthorize

--- config/application.config.php
+++ config/application.config.php
@@ -3,7 +3,11 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
-    	'Album',
+        'ZendDeveloperTools',
+        'ZfcBase',
+        'ZfcUser',
+        'BjyAuthorize',
+    	'Album'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
