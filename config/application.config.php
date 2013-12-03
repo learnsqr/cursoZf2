222c3f8f7959d957136cb17cabcda89bf74768ec
Alejandro <my@email.com>
2013-12-03 20:29:47 +0100

    ZfcUserAdmin

--- config/application.config.php
+++ config/application.config.php
@@ -2,8 +2,14 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
+        'ZendDeveloperTools',
         'Application',
         'Album',
+        'BjyProfiler',
+        'ZfcBase',
+        'ZfcUser',
+        'ZfcAdmin',
+        'ZfcUserAdmin',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
