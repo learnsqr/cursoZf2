b8873ffc5529d4920cc6dc4c0abb1dac365fd66f
Agustin Calderon <agustincl@gmail.com>
2014-03-14 19:35:13 +0100

    Global.php changed and local.php.dist added for mockup

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,29 +1,28 @@
 <?php
-
-
-$dbParams = array(
-    'database'  => 'zf2',
-    'username'  => 'php',
-    'password'  => '1234',
-    'hostname'  => 'localhost',
-);
+/**
+ * Global Configuration Override
+ *
+ * You can use this file for overriding configuration values from modules, etc.
+ * You would place values in here that are agnostic to the environment and not
+ * sensitive to security.
+ *
+ * @NOTE: In practice, this file will typically be INCLUDED in your source
+ * control, so do not include passwords or other sensitive information in this
+ * file.
+ */
 
 return array(
-		'db' => array(
-				'driver'         => 'Pdo',
-				'dsn'            => 'mysql:dbname=zf2;host=localhost',
-					'database'  => $dbParams['database'],
-                    'username'  => $dbParams['username'],
-                    'password'  => $dbParams['password'],
-                    'hostname'  => $dbParams['hostname'],
-				'driver_options' => array(
-						PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
-				),
-		),
-		'service_manager' => array(
-				'factories' => array(
-						'Zend\Db\Adapter\Adapter'
-						=> 'Zend\Db\Adapter\AdapterServiceFactory',
-				),
-		),
-);
+     'db' => array(
+         'driver'         => 'Pdo',
+         'dsn'            => 'mysql:dbname=orkestor;host=localhost',
+         'driver_options' => array(
+             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+         ),
+     ),
+     'service_manager' => array(
+         'factories' => array(
+             'Zend\Db\Adapter\Adapter'
+                     => 'Zend\Db\Adapter\AdapterServiceFactory',
+         ),
+     ),
+ );
