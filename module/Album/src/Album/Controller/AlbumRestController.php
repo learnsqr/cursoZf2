5b9e57939cbe163c3dd8b4170f7078d07cfef367
Agustin Calderon <agustincl@gmail.com>
2014-03-19 15:40:10 +0100

    Test curl added to controller rest

--- module/Album/src/Album/Controller/AlbumRestController.php
+++ module/Album/src/Album/Controller/AlbumRestController.php
@@ -10,10 +10,22 @@ class AlbumRestController extends AbstractRestfulController
     
     protected $albumTable;
     
+    /**
+     * test with
+     * curl -i -H "Accept: application/json" http://zf2.local/album-rest/1
+     * (non-PHPdoc)
+     * @see \Zend\Mvc\Controller\AbstractRestfulController::get()
+     */
     public function get($id) {
         return new JsonModel(array("album" => $this->getAlbumTable()->getAlbum($id)));
     }
     
+    /**
+     * Test with
+     * curl -i -H "Accept: application/json" http://zf2.local/album-rest
+     * (non-PHPdoc)
+     * @see \Zend\Mvc\Controller\AbstractRestfulController::getList()
+     */
     public function getList() {
         $albums = $this->getAlbumTable()->fetchAll();
         $data = array();
@@ -25,6 +37,12 @@ class AlbumRestController extends AbstractRestfulController
         ));
     }
     
+    /**
+     * Test with
+     * curl -i -H "Accept: application/json" -X PUT -d "artist=New 2&title=New Title 2" http://zf2.local/album-rest/2
+     * (non-PHPdoc)
+     * @see \Zend\Mvc\Controller\AbstractRestfulController::update()
+     */
     public function update($id, $data) {
         $album = $this->getAlbumTable()->getAlbum($id);
         foreach ($data as $key => $value) {
@@ -34,6 +52,12 @@ class AlbumRestController extends AbstractRestfulController
         return new JsonModel(array("status" => "ok"));
     }
     
+    /**
+     * Test With 
+     * curl -i -H "Accept: application/json" -X POST -d "artist=Some Name&title=Some Title" http://zf2.local/album-rest
+     * (non-PHPdoc)
+     * @see \Zend\Mvc\Controller\AbstractRestfulController::create()
+     */
     function create($data) {
         $album = new Album();
         $album->exchangeArray($data);
@@ -41,6 +65,12 @@ class AlbumRestController extends AbstractRestfulController
         return new JsonModel(array("status" => "ok"));
     }
     
+    /**
+     * Test with
+     * curl -i -H "Accept: application/json" -X DELETE http://zf2.local/album-rest/14
+     * (non-PHPdoc)
+     * @see \Zend\Mvc\Controller\AbstractRestfulController::delete()
+     */
     public function delete($id) {
         $this->getAlbumTable()->deleteAlbum($id);
         return new JsonModel(array("status" => "ok"));
