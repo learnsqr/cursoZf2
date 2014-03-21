68e8a7c92c91db1bf05625942d86a428b4b6eb9d
Agustin Calderon <agustincl@gmail.com>
2014-03-21 12:50:12 +0100

    Database updated with task_list

--- data/database_data.sql
+++ data/database_data.sql
@@ -16,6 +16,132 @@ USE `zf2`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `album`
+--
+
+LOCK TABLES `album` WRITE;
+/*!40000 ALTER TABLE `album` DISABLE KEYS */;
+INSERT INTO `album` VALUES (2,'New 2','New Title 2'),(3,'Bruce  Springsteen','Wrecking Ball (Deluxe)'),(4,'Lana  Del  Rey','Born  To  Die'),(5,'Gotye','Making  Mirrors'),(10,'s','s'),(11,'asdfasd','dxasdf');
+/*!40000 ALTER TABLE `album` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `oauth_access_tokens`
+--
+
+LOCK TABLES `oauth_access_tokens` WRITE;
+/*!40000 ALTER TABLE `oauth_access_tokens` DISABLE KEYS */;
+INSERT INTO `oauth_access_tokens` VALUES ('10671442ee7dac29e3cc3c9cbfcae8e7200f314e','testclient',NULL,'2014-03-20 21:24:15',NULL),('11f939c6a0530070a9f1c58c503e20eda48a1c53','testclient','testclient','2014-03-20 21:27:36',NULL),('140ce662c444cb40a0a20effaa37866e44538165','testclient','testclient','2014-03-20 21:25:31',NULL),('1af85611751764f4eba99cda8791cd3936f94f06','testclient',NULL,'2014-03-20 21:27:08',NULL),('50df9003f3ef2af5d628386683c3df9697e533a4','testclient',NULL,'2014-03-20 21:17:29',NULL),('597dd381d516470ff220ce5a1e1db5a593c2faa3','testclient',NULL,'2014-03-20 21:17:34',NULL),('6e404e9cd0e7181344db399deee247e09cfb726f','testclient',NULL,'2014-03-20 21:24:11',NULL),('7ffc325852a11b5f36633f395fc82c311b382e47','testclient','testclient','2014-03-20 21:30:25',NULL),('ac3246ce98ccadf3241269abb7f0bb6ef5ee88b6','testclient',NULL,'2014-03-20 21:17:06',NULL),('b99d9995c425097afe5d488cd1e67e1c2a2ba95f','testclient','testclient','2014-03-20 21:26:08',NULL),('e4e9aa201e76ef2433db3bb58964e55d69950441','testclient',NULL,'2014-03-20 21:17:15',NULL);
+/*!40000 ALTER TABLE `oauth_access_tokens` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `oauth_authorization_codes`
+--
+
+LOCK TABLES `oauth_authorization_codes` WRITE;
+/*!40000 ALTER TABLE `oauth_authorization_codes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oauth_authorization_codes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `oauth_clients`
+--
+
+LOCK TABLES `oauth_clients` WRITE;
+/*!40000 ALTER TABLE `oauth_clients` DISABLE KEYS */;
+INSERT INTO `oauth_clients` VALUES ('testclient','$2y$14$f3qml4G2hG6sxM26VMq.geDYbsS089IBtVJ7DlD05BoViS9PFykE2','/oauth/receivecode',NULL,NULL,NULL);
+/*!40000 ALTER TABLE `oauth_clients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `oauth_jwt`
+--
+
+LOCK TABLES `oauth_jwt` WRITE;
+/*!40000 ALTER TABLE `oauth_jwt` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oauth_jwt` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `oauth_refresh_tokens`
+--
+
+LOCK TABLES `oauth_refresh_tokens` WRITE;
+/*!40000 ALTER TABLE `oauth_refresh_tokens` DISABLE KEYS */;
+INSERT INTO `oauth_refresh_tokens` VALUES ('36995417725ff744bdbe287b48fac8fb8e16b47e','testclient','testclient','2014-04-03 19:26:08',NULL),('3ca0d2966de56e4e3d70b709e92160ff13beeaf7','testclient','testclient','2014-04-03 19:27:36',NULL),('eca1649669d2bb2619171cbd81e13b98f32a81a8','testclient','testclient','2014-04-03 19:30:25',NULL),('f7bb74f5da2e359235c32328c8bdafc4a0161393','testclient','testclient','2014-04-03 19:25:31',NULL);
+/*!40000 ALTER TABLE `oauth_refresh_tokens` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `oauth_scopes`
+--
+
+LOCK TABLES `oauth_scopes` WRITE;
+/*!40000 ALTER TABLE `oauth_scopes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oauth_scopes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `oauth_users`
+--
+
+LOCK TABLES `oauth_users` WRITE;
+/*!40000 ALTER TABLE `oauth_users` DISABLE KEYS */;
+INSERT INTO `oauth_users` VALUES ('testclient','$2y$14$f3qml4G2hG6sxM26VMq.geDYbsS089IBtVJ7DlD05BoViS9PFykE2','Test','User');
+/*!40000 ALTER TABLE `oauth_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `task_item`
+--
+
+LOCK TABLES `task_item` WRITE;
+/*!40000 ALTER TABLE `task_item` DISABLE KEYS */;
+INSERT INTO `task_item` VALUES (1,'My First Task',0,'2014-03-13 00:00:00'),(2,'My Second Task',1,'2014-03-13 00:00:00');
+/*!40000 ALTER TABLE `task_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,NULL,'agustincl@gmail.com',NULL,'$2y$14$i0pmvl8J/nM6jf09aRq0IuCBxeqnglvI8dpK/KnsEyt/oDK54D0Mm',NULL),(2,NULL,'asdasd@asdasd.com',NULL,'$2y$14$r228bs8hI7nvntTWyZ6ZnekQW7TIBZAvZt9Lbkxpx97YUwPgKExi2',NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `user_provider`
+--
+
+LOCK TABLES `user_provider` WRITE;
+/*!40000 ALTER TABLE `user_provider` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_provider` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `user_role`
+--
+
+LOCK TABLES `user_role` WRITE;
+/*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
+INSERT INTO `user_role` VALUES (4,'admin',1,NULL);
+/*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `user_role_linker`
+--
+
+LOCK TABLES `user_role_linker` WRITE;
+/*!40000 ALTER TABLE `user_role_linker` DISABLE KEYS */;
+INSERT INTO `user_role_linker` VALUES (1,'4');
+/*!40000 ALTER TABLE `user_role_linker` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -26,4 +152,4 @@ USE `zf2`;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-03-20 21:46:49
+-- Dump completed on 2014-03-21 12:48:24
--- data/database_data_structure.sql
+++ data/database_data_structure.sql
@@ -29,7 +29,7 @@ CREATE TABLE `album` (
   `artist` varchar(100) NOT NULL,
   `title` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,8 +38,217 @@ CREATE TABLE `album` (
 
 LOCK TABLES `album` WRITE;
 /*!40000 ALTER TABLE `album` DISABLE KEYS */;
-INSERT INTO `album` VALUES (2,'Ac-Dc','Dirty Deeds'),(3,'Bruce  Springsteen','Wrecking Ball (Deluxe)'),(4,'Lana  Del  Rey','Born  To  Die'),(5,'Gotye','Making  Mirrors');
+INSERT INTO `album` VALUES (2,'New 2','New Title 2'),(3,'Bruce  Springsteen','Wrecking Ball (Deluxe)'),(4,'Lana  Del  Rey','Born  To  Die'),(5,'Gotye','Making  Mirrors'),(10,'s','s'),(11,'asdfasd','dxasdf');
 /*!40000 ALTER TABLE `album` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `oauth_access_tokens`
+--
+
+DROP TABLE IF EXISTS `oauth_access_tokens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oauth_access_tokens` (
+  `access_token` varchar(40) NOT NULL,
+  `client_id` varchar(80) NOT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `scope` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`access_token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oauth_access_tokens`
+--
+
+LOCK TABLES `oauth_access_tokens` WRITE;
+/*!40000 ALTER TABLE `oauth_access_tokens` DISABLE KEYS */;
+INSERT INTO `oauth_access_tokens` VALUES ('10671442ee7dac29e3cc3c9cbfcae8e7200f314e','testclient',NULL,'2014-03-20 21:24:15',NULL),('11f939c6a0530070a9f1c58c503e20eda48a1c53','testclient','testclient','2014-03-20 21:27:36',NULL),('140ce662c444cb40a0a20effaa37866e44538165','testclient','testclient','2014-03-20 21:25:31',NULL),('1af85611751764f4eba99cda8791cd3936f94f06','testclient',NULL,'2014-03-20 21:27:08',NULL),('50df9003f3ef2af5d628386683c3df9697e533a4','testclient',NULL,'2014-03-20 21:17:29',NULL),('597dd381d516470ff220ce5a1e1db5a593c2faa3','testclient',NULL,'2014-03-20 21:17:34',NULL),('6e404e9cd0e7181344db399deee247e09cfb726f','testclient',NULL,'2014-03-20 21:24:11',NULL),('7ffc325852a11b5f36633f395fc82c311b382e47','testclient','testclient','2014-03-20 21:30:25',NULL),('ac3246ce98ccadf3241269abb7f0bb6ef5ee88b6','testclient',NULL,'2014-03-20 21:17:06',NULL),('b99d9995c425097afe5d488cd1e67e1c2a2ba95f','testclient','testclient','2014-03-20 21:26:08',NULL),('e4e9aa201e76ef2433db3bb58964e55d69950441','testclient',NULL,'2014-03-20 21:17:15',NULL);
+/*!40000 ALTER TABLE `oauth_access_tokens` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `oauth_authorization_codes`
+--
+
+DROP TABLE IF EXISTS `oauth_authorization_codes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oauth_authorization_codes` (
+  `authorization_code` varchar(40) NOT NULL,
+  `client_id` varchar(80) NOT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `redirect_uri` varchar(2000) DEFAULT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `scope` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`authorization_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oauth_authorization_codes`
+--
+
+LOCK TABLES `oauth_authorization_codes` WRITE;
+/*!40000 ALTER TABLE `oauth_authorization_codes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oauth_authorization_codes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `oauth_clients`
+--
+
+DROP TABLE IF EXISTS `oauth_clients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oauth_clients` (
+  `client_id` varchar(80) NOT NULL,
+  `client_secret` varchar(80) NOT NULL,
+  `redirect_uri` varchar(2000) NOT NULL,
+  `grant_types` varchar(80) DEFAULT NULL,
+  `scope` varchar(2000) DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oauth_clients`
+--
+
+LOCK TABLES `oauth_clients` WRITE;
+/*!40000 ALTER TABLE `oauth_clients` DISABLE KEYS */;
+INSERT INTO `oauth_clients` VALUES ('testclient','$2y$14$f3qml4G2hG6sxM26VMq.geDYbsS089IBtVJ7DlD05BoViS9PFykE2','/oauth/receivecode',NULL,NULL,NULL);
+/*!40000 ALTER TABLE `oauth_clients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `oauth_jwt`
+--
+
+DROP TABLE IF EXISTS `oauth_jwt`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oauth_jwt` (
+  `client_id` varchar(80) NOT NULL,
+  `subject` varchar(80) DEFAULT NULL,
+  `public_key` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oauth_jwt`
+--
+
+LOCK TABLES `oauth_jwt` WRITE;
+/*!40000 ALTER TABLE `oauth_jwt` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oauth_jwt` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `oauth_refresh_tokens`
+--
+
+DROP TABLE IF EXISTS `oauth_refresh_tokens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oauth_refresh_tokens` (
+  `refresh_token` varchar(40) NOT NULL,
+  `client_id` varchar(80) NOT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `scope` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`refresh_token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oauth_refresh_tokens`
+--
+
+LOCK TABLES `oauth_refresh_tokens` WRITE;
+/*!40000 ALTER TABLE `oauth_refresh_tokens` DISABLE KEYS */;
+INSERT INTO `oauth_refresh_tokens` VALUES ('36995417725ff744bdbe287b48fac8fb8e16b47e','testclient','testclient','2014-04-03 19:26:08',NULL),('3ca0d2966de56e4e3d70b709e92160ff13beeaf7','testclient','testclient','2014-04-03 19:27:36',NULL),('eca1649669d2bb2619171cbd81e13b98f32a81a8','testclient','testclient','2014-04-03 19:30:25',NULL),('f7bb74f5da2e359235c32328c8bdafc4a0161393','testclient','testclient','2014-04-03 19:25:31',NULL);
+/*!40000 ALTER TABLE `oauth_refresh_tokens` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `oauth_scopes`
+--
+
+DROP TABLE IF EXISTS `oauth_scopes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oauth_scopes` (
+  `type` varchar(255) NOT NULL DEFAULT 'supported',
+  `scope` varchar(2000) DEFAULT NULL,
+  `client_id` varchar(80) DEFAULT NULL,
+  `is_default` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oauth_scopes`
+--
+
+LOCK TABLES `oauth_scopes` WRITE;
+/*!40000 ALTER TABLE `oauth_scopes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oauth_scopes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `oauth_users`
+--
+
+DROP TABLE IF EXISTS `oauth_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oauth_users` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(2000) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oauth_users`
+--
+
+LOCK TABLES `oauth_users` WRITE;
+/*!40000 ALTER TABLE `oauth_users` DISABLE KEYS */;
+INSERT INTO `oauth_users` VALUES ('testclient','$2y$14$f3qml4G2hG6sxM26VMq.geDYbsS089IBtVJ7DlD05BoViS9PFykE2','Test','User');
+/*!40000 ALTER TABLE `oauth_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task_item`
+--
+
+DROP TABLE IF EXISTS `task_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task_item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `completed` tinyint(4) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task_item`
+--
+
+LOCK TABLES `task_item` WRITE;
+/*!40000 ALTER TABLE `task_item` DISABLE KEYS */;
+INSERT INTO `task_item` VALUES (1,'My First Task',0,'2014-03-13 00:00:00'),(2,'My Second Task',1,'2014-03-13 00:00:00');
+/*!40000 ALTER TABLE `task_item` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -162,4 +371,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-12-03 21:06:41
+-- Dump completed on 2014-03-21 12:49:07
