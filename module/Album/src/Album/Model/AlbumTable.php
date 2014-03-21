73bc07b62a4ad3df1de665c7ab1b99ed6aaf967f
Agustin Calderon <agustincl@gmail.com>
2014-03-21 12:54:34 +0100

    Album tablegateway pagination added

--- module/Album/src/Album/Model/AlbumTable.php
+++ module/Album/src/Album/Model/AlbumTable.php
@@ -1,7 +1,11 @@
 <?php
 namespace Album\Model;
 
+use Zend\Db\ResultSet\ResultSet;
 use Zend\Db\TableGateway\TableGateway;
+use Zend\Db\Sql\Select;
+use Zend\Paginator\Adapter\DbSelect;
+use Zend\Paginator\Paginator;
 
 class AlbumTable
 {
@@ -12,10 +16,34 @@ class AlbumTable
         $this->tableGateway = $tableGateway;
     }
 
-    public function fetchAll()
+//     public function fetchAll()
+//     {
+//         $resultSet = $this->tableGateway->select();
+//         return $resultSet;
+//     }
+    
+    public function fetchAll($paginated=false)
     {
-        $resultSet = $this->tableGateway->select();
-        return $resultSet;
+    	if ($paginated) {
+    		// create a new Select object for the table album
+    		$select = new Select('album');
+    		// create a new result set based on the Album entity
+    		$resultSetPrototype = new ResultSet();
+    		$resultSetPrototype->setArrayObjectPrototype(new Album());
+    		// create a new pagination adapter object
+    		$paginatorAdapter = new DbSelect(
+    				// our configured select object
+    				$select,
+    				// the adapter to run it against
+    				$this->tableGateway->getAdapter(),
+    				// the result set to hydrate
+    				$resultSetPrototype
+    		);
+    		$paginator = new Paginator($paginatorAdapter);
+    		return $paginator;
+    	}
+    	$resultSet = $this->tableGateway->select();
+    	return $resultSet;
     }
 
     public function getAlbum($id)
