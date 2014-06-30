783b765708c4dd498cb7d509892c33cce6a60c5a
Agustín F. Calderón M. <agustincl@gmail.com>
2014-06-30 16:15:26 +0200

    view_helper_config added to application Module Config to use flashMessenger

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -123,5 +123,12 @@ return array(
             ),
         ),
     ),
+    'view_helper_config' => array(
+    		'flashmessenger' => array(
+    				'message_open_format'      => '<div%s><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><ul><li>',
+    				'message_close_string'     => '</li></ul></div>',
+    				'message_separator_string' => '</li><li>'
+    		)
+    )
     
 );
