0db2a30f4cd7cc1df2aecc78204149b031b86c6a
Agustín F. Calderón M. <agustincl@gmail.com>
2014-07-03 18:17:41 +0200

    API Music V2 added. Only delete authentication.

--- module/Music/config/module.config.php
+++ module/Music/config/module.config.php
@@ -17,6 +17,7 @@ return array(
         'uri' => array(
             0 => 'music.rest.album',
         ),
+        'default_version' => 2,
     ),
     'service_manager' => array(
         'factories' => array(),
@@ -45,10 +46,33 @@ return array(
             'collection_class' => 'Music\\V1\\Rest\\Album\\AlbumCollection',
             'service_name' => 'Album',
         ),
+        'Music\\V2\\Rest\\Album\\Controller' => array(
+            'listener' => 'Music\\V2\\Rest\\Album\\AlbumResource',
+            'route_name' => 'music.rest.album',
+            'route_identifier_name' => 'album_id',
+            'collection_name' => 'album',
+            'entity_http_methods' => array(
+                0 => 'GET',
+                1 => 'PATCH',
+                2 => 'PUT',
+                3 => 'DELETE',
+            ),
+            'collection_http_methods' => array(
+                0 => 'GET',
+                1 => 'POST',
+            ),
+            'collection_query_whitelist' => array(),
+            'page_size' => '25',
+            'page_size_param' => '',
+            'entity_class' => 'Music\\V2\\Rest\\Album\\AlbumEntity',
+            'collection_class' => 'Music\\V2\\Rest\\Album\\AlbumCollection',
+            'service_name' => 'Album',
+        ),
     ),
     'zf-content-negotiation' => array(
         'controllers' => array(
             'Music\\V1\\Rest\\Album\\Controller' => 'HalJson',
+            'Music\\V2\\Rest\\Album\\Controller' => 'HalJson',
         ),
         'accept_whitelist' => array(
             'Music\\V1\\Rest\\Album\\Controller' => array(
@@ -56,10 +80,19 @@ return array(
                 1 => 'application/hal+json',
                 2 => 'application/json',
             ),
+            'Music\\V2\\Rest\\Album\\Controller' => array(
+                0 => 'application/vnd.music.v2+json',
+                1 => 'application/hal+json',
+                2 => 'application/json',
+            ),
         ),
         'content_type_whitelist' => array(
             'Music\\V1\\Rest\\Album\\Controller' => array(
                 0 => 'application/vnd.music.v1+json',
+                1 => 'application/json',
+            ),
+            'Music\\V2\\Rest\\Album\\Controller' => array(
+                0 => 'application/vnd.music.v2+json',
                 1 => 'application/json',
             ),
         ),
@@ -78,11 +111,26 @@ return array(
                 'route_identifier_name' => 'album_id',
                 'is_collection' => true,
             ),
+            'Music\\V2\\Rest\\Album\\AlbumEntity' => array(
+                'entity_identifier_name' => 'id',
+                'route_name' => 'music.rest.album',
+                'route_identifier_name' => 'album_id',
+                'hydrator' => 'Zend\\Stdlib\\Hydrator\\ClassMethods',
+            ),
+            'Music\\V2\\Rest\\Album\\AlbumCollection' => array(
+                'entity_identifier_name' => 'id',
+                'route_name' => 'music.rest.album',
+                'route_identifier_name' => 'album_id',
+                'is_collection' => '1',
+            ),
         ),
     ),
     'zf-content-validation' => array(
         'Music\\V1\\Rest\\Album\\Controller' => array(
             'input_filter' => 'Music\\V1\\Rest\\Album\\Validator',
+        ),
+        'Music\\V2\\Rest\\Album\\Controller' => array(
+            'input_filter' => 'Music\\V2\\Rest\\Album\\Validator',
         ),
     ),
     'input_filter_specs' => array(
@@ -132,6 +180,74 @@ return array(
                     ),
                 ),
                 'description' => 'Artist who made this album!',
+            ),
+        ),
+        'Music\\V2\\Rest\\Album\\Validator' => array(
+            0 => array(
+                'name' => 'title',
+                'required' => '1',
+                'filters' => array(
+                    0 => array(
+                        'name' => 'Zend\\Filter\\StringTrim',
+                        'options' => array(),
+                    ),
+                    1 => array(
+                        'name' => 'Zend\\Filter\\StripTags',
+                        'options' => array(),
+                    ),
+                ),
+                'validators' => array(
+                    0 => array(
+                        'name' => 'Zend\\Validator\\StringLength',
+                        'options' => array(
+                            'max' => '100',
+                        ),
+                    ),
+                ),
+                'description' => 'Title of this album',
+            ),
+            1 => array(
+                'name' => 'artist',
+                'required' => '1',
+                'filters' => array(
+                    0 => array(
+                        'name' => 'Zend\\Filter\\StringTrim',
+                        'options' => array(),
+                    ),
+                    1 => array(
+                        'name' => 'Zend\\Filter\\StripTags',
+                        'options' => array(),
+                    ),
+                ),
+                'validators' => array(
+                    0 => array(
+                        'name' => 'Zend\\Validator\\StringLength',
+                        'options' => array(
+                            'max' => '100',
+                        ),
+                    ),
+                ),
+                'description' => 'Artist who made this album!',
+            ),
+        ),
+    ),
+    'zf-mvc-auth' => array(
+        'authorization' => array(
+            'Music\\V2\\Rest\\Album\\Controller' => array(
+                'entity' => array(
+                    'GET' => false,
+                    'POST' => false,
+                    'PATCH' => false,
+                    'PUT' => false,
+                    'DELETE' => true,
+                ),
+                'collection' => array(
+                    'GET' => false,
+                    'POST' => false,
+                    'PATCH' => false,
+                    'PUT' => false,
+                    'DELETE' => false,
+                ),
             ),
         ),
     ),
--- module/Music/src/Music/Module.php
+++ module/Music/src/Music/Module.php
@@ -35,6 +35,16 @@ class Module implements ApigilityProviderInterface
     						$mapper = $sm->get('Music\V1\Rest\Album\AlbumMapper');
     						return new \Music\V1\Rest\Album\AlbumResource($mapper);
     					},
+    					'Music\V2\Rest\Album\AlbumMapper' =>  function ($sm) {
+    						$adapter 		= $sm->get('Zend\Db\Adapter\Adapter');
+    						$adapterMaster 	= $sm->get('dbMasterAdapter');
+    						$adapterSlave 	= $sm->get('dbSlaveAdapter');
+    						return new \Music\V2\Rest\Album\AlbumMapper($adapterMaster, $adapterSlave);
+    					},
+    					'Music\V2\Rest\Album\AlbumResource' => function ($sm) {
+    						$mapper = $sm->get('Music\V2\Rest\Album\AlbumMapper');
+    						return new \Music\V2\Rest\Album\AlbumResource($mapper);
+    					},
     			),
     	);
     }
